c42609799cba801c2827b48a4529ed98e396ea5f
Haleigh <schwartzh2017@gmail.com>
2024-06-17 12:46:49 -0700

    correct mistake with make_tsv.py and try again with output.tsv

--- make_tsv.py
+++ make_tsv.py
@@ -8,8 +8,9 @@ OUTPUT_FILE = "/Users/Haleigh/Documents/MSDS/Cloud_Computing/week5/output.tsv" #
 
 
 with open(OUTPUT_FILE, "w") as output_file: #open the output file in write mode
+    output_file.write("TsvHttpData-1.0\n") #write the header to the output file, don't want it to repeat with each instance of the loop
 
-    for i in os.listdir(BK_DIR):
+    for i in os.listdir(BK_DIR): #go through each file in directory
         if i.endswith(".txt"): #check if the file is a text file
             path = os.path.join(BK_DIR, i) #get full path of file
             with open(path, "r") as f: #open the file in read mode
@@ -24,4 +25,4 @@ with open(OUTPUT_FILE, "w") as output_file: #open the output file in write mode
                 f.seek(0, os.SEEK_END) #go to the end of the file
                 size = f.tell() #find size of file in bytes
 
-                output_file.write("TsvHttpData-1.0\n"f"{PREFIX}{i}\t{size}\t{b64}\n") #save raw github content url, size of file, and base64 encoded md5 hash to the output file
+                output_file.write(f"{PREFIX}{i}\t{size}\t{b64}\n") #save raw github content url, size of file, and base64 encoded md5 hash to the output file
